a4bfe4f1b32f2c974d4ef8d39b08a058f5680379
LimBoo233 <13720081217@163.com>
2025-03-30 19:14:02 +0800

    游戏主类

--- src/com/group7/main/java/ForbiddenIslandGame.java
+++ src/com/group7/main/java/ForbiddenIslandGame.java
@@ -1,8 +1,15 @@
+import controller.GameController;
 import model.*;
 import model.enums.*;
+import view.MainFrame;
 
 public class ForbiddenIslandGame {
     public static void main(String[] args) {
+        Game game = new Game();
+        MainFrame mainFrame = new MainFrame();
+        GameController gameController = new GameController(game, mainFrame);
 
+        gameController.initializeViewFrame();
+        gameController.startGame();
     }
 }
